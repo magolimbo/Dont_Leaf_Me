06cfc937867586c5b5638ec3de6dd28ba9aa0a40
magolimbo / Gabriele Turetta <turetta6@gmail.com>
2024-03-27 18:46:18 +0100

    working on go back button to home screen

--- App/Screens/AddPlantScreen/AddPlant.jsx
+++ App/Screens/AddPlantScreen/AddPlant.jsx
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import Colors from '../../Utils/Colors'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons';
 
 export default function AddPlant() {
   return (
     <SafeAreaView style={{flex:1}}>
         <View style={styles.container}>
-            <Text>AddPlant</Text>
+            {/* button to go back to home screen */}
+            <TouchableOpacity>
+                <View style={styles.buttonBack}>
+                    <Feather name="arrow-left" size={24} color={Colors.WHITE} />
+                </View>     
+            </TouchableOpacity>
         </View>
     </SafeAreaView>
   )
@@ -19,5 +27,13 @@ const styles = StyleSheet.create({
         padding: 25,
         borderColor: 'red',
         borderWidth: 1
+    },
+    buttonBack:{
+        backgroundColor: Colors.ORANGE,
+        padding: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        width: 50,
+        radius: 4
     }
 })
