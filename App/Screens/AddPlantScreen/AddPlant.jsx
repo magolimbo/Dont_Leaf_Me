705c8efd8999db860d9cc2d4b5b386f18dca9e5a
Gabriellanaro <s233541@dtu.dk>
2024-04-03 14:48:46 +0200

    Camera working

--- App/Screens/AddPlantScreen/AddPlant.jsx
+++ App/Screens/AddPlantScreen/AddPlant.jsx
@@ -1,7 +1,7 @@
 import { Button, StyleSheet, Text, View} from 'react-native'
 import {Picker} from '@react-native-picker/picker';
 import { SafeAreaView } from 'react-native-safe-area-context'
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Colors from '../../Utils/Colors'
 import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons';
@@ -10,24 +10,89 @@ import { Raleway_400Regular } from "@expo-google-fonts/raleway";
 import { Raleway_800ExtraBold } from "@expo-google-fonts/raleway";
 import { useFonts } from "expo-font";
 import { AntDesign } from '@expo/vector-icons';
-import { useRef } from 'react';
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraType, requestCameraPermissionsAsync } from 'expo-camera';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
 export default function AddPlant({ navigation }) {
 
-    const [type, setType] = useState(CameraType.back);
     const [hasPermission, setHasPermission] = useState(null);
-
-    useEffect(() => {
-        (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-    }, []);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [cameraVisible, setCameraVisible] = useState(false);
+    const cameraRef = useRef(null);
+    const [profilePictureUri, setProfilePictureUri] = useState(null);
+    const [selectedValue, setSelectedValue] = useState("option1");
     const [fontsLoaded] = useFonts({
         Raleway_400Regular,
         Raleway_800ExtraBold
-      });
+    });
+
+    const toggleCamera = async () => {
+        // Capture image and save to device storage
+        if (!hasPermission) {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        }
+        if (!cameraVisible) {
+            setCameraVisible(true);
+        }
+        else {
+            setCameraVisible(false);
+        }
+    };
+
+    const capturePhoto = async () => {
+        try {
+            if (cameraRef.current) {
+                const photo = await cameraRef.current.takePictureAsync();
+                if (photo.uri) {
+                    setCapturedImageUri(photo.uri);
+                    await AsyncStorage.setItem('profilePictureUri', photo.uri);
+                    console.log('Profile picture URI saved successfully:', photo.uri);
+                } else {
+                    console.error('Failed to capture photo: URI is undefined');
+                }
+            } else {
+                console.error('Camera reference is not defined');
+            }
+        } catch (error) {
+            console.error('Error capturing photo:', error);
+        }
+    };
+
+    const toggleCameraType = () => {
+        setType(
+            type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+        );
+    };
+
+
+    useEffect(() => {
+        (async () => {
+            // Controlla i permessi della fotocamera
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+
+            // Recupera l'URI dell'immagine salvata dall'AsyncStorage
+            try {
+                const uri = await AsyncStorage.getItem('profilePictureUri');
+                if (uri) {
+                    setProfilePictureUri(uri);
+                }
+            } catch (error) {
+                console.error('Error retrieving profile picture URI:', error);
+            }
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     
     if (!fontsLoaded) {
     return (
@@ -35,11 +100,6 @@ export default function AddPlant({ navigation }) {
     )
     }
 
-    const [selectedValue, setSelectedValue] = useState("option1");
-
-    function toggleCameraType() {
-        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-    }
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -66,6 +126,16 @@ export default function AddPlant({ navigation }) {
                     {/* PROFILE PICTURE */}
                     <View style={{alignItems: 'center'}}>
                         <View style={styles.propic}>
+                            {/* Se è presente un'immagine come profile picture, mostra l'immagine */}
+                            {profilePictureUri && (
+                                <TouchableOpacity onPress={() => { uri }}>
+                                    <Image source={{ uri: profilePictureUri }} style={{ width: 100, height: 100, borderRadius: 50 }} />
+                                </TouchableOpacity>
+                            )}
+                            {/* Messaggio di fallback nel caso in cui non ci sia una foto profilo */}
+                            {!profilePictureUri && (
+                                <Text>No profile picture found</Text>
+                            )}
                         </View>
                         <Text style={{fontFamily:'Raleway_400Regular', color: Colors.WHITE, marginBottom: 30}}>Pro Pic</Text>
                     </View>
@@ -151,7 +221,6 @@ export default function AddPlant({ navigation }) {
                     </View>
 
 
-                    
 
                     <View style={{marginBottom: 20, justifyContent: 'center'}}>
                         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
@@ -190,15 +259,36 @@ export default function AddPlant({ navigation }) {
                     </View>
                     {/* Block for camera */}
                     <View style={{ flex: 1, marginLeft: 40, marginTop: 10 }}>
-                        <View style={styles.propic}>
-                            <Camera style={styles.camera} type={type}>
-                                <View style={styles.buttonContainer}>
-                                    <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-                                        <Text style={styles.text}>Flip Camera</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </Camera>
-                        </View>
+                        {/* Button to open/close camera */}
+                        <TouchableOpacity onPress={toggleCamera}>
+                            <View style={{ backgroundColor: Colors.ORANGE, padding: 10 }}>
+                                <Text style={{ color: Colors.WHITE, alignItems: 'center', alignSelf: 'center' }}>{cameraVisible ? 'Close Camera' : 'Open Camera'}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        {/* Render camera if visible */}
+                        {cameraVisible && (
+                            <View style={{ flex: 1 }}>
+                                <Camera style={{ flex: 1 }} type={type}>
+                                    <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+                                        <TouchableOpacity
+                                            style={{
+                                                flex: 0.1,
+                                                alignSelf: 'flex-end',
+                                                alignItems: 'center',
+                                            }}
+                                            onPress={toggleCameraType}
+                                        >
+                                            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </Camera>
+                                {/* Button to capture photo */}
+                                <TouchableOpacity onPress={capturePhoto} style={{ position: 'absolute', bottom: 20, alignSelf: 'center' }}>
+                                    <Text style={{ fontSize: 18, color: 'white' }}>Capture Photo</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
                     </View>
                 </View>
 
